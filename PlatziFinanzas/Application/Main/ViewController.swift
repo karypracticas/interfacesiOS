ff35918a425253607dcde99a112751e871693dc1
Kary Rguez <practicasgithub@gmail.com>
2020-05-20 11:49:29 -0500

    Practica interfaces iOS, viewcontroller Login

---
+++ PlatziFinanzas/Application/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Kary on 05/05/20.
+//  Copyright © 2020 Kary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
